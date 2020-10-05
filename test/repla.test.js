7e3f1912d2452129b073c69f635122b8dc688200
Roben Kleene <contact@robenkleene.com>
2020-10-05 17:34:00 -0400

    Fix class name

--- test/repla.test.js
+++ test/repla.test.js
@@ -1,6 +1,7 @@
-import window from "../src/window.js"
+import Window from "../src/window.js"
 
 test("adds 1 + 2 to equal 3", () => {
+  var window = new Window(); 
   let result = window.loadUrl()
   console.log("result = " + result);
   expect(result).toBe(null);
