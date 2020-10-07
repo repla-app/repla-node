33eafeea728dfbf645a6db8e3f6bf4628b624c43
Roben Kleene <contact@robenkleene.com>
2020-10-07 18:12:16 -0400

    Setup run AppleScript

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const WINDOW_ID_KEY = 'REPLA_WINDOW_ID';
+export const APPLESCRIPT_COMMAND = "/usr/bin/osascript";
--- src/repla.js
+++ src/repla.js
@@ -1,5 +1,9 @@
+import { APPLESCRIPT_COMMAND } from "../src/constants.js";
+
 export default Repla = {
   createWindow: function() {
     
+  },
+  _runAppleScript: function(args) {
   }
 }
