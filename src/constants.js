728fc8909cafa2974bb04835f73ae808262d3b68
Roben Kleene <contact@robenkleene.com>
2020-10-13 17:44:37 -0400

    Add dirname manually

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,9 @@ export const WINDOW_ID_KEY = "REPLA_WINDOW_ID";
 import path from "path";
 
 // AppleScript
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const APPLESCRIPT_DIRECTORY = path.join(__dirname, "lib/applescript");
 export const APPLESCRIPT_COMMAND = "/usr/bin/osascript";
 export const CREATE_WINDOW_SCRIPT = path.join(
