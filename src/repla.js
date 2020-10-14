4212fbf956d3e6b3f181eb910c733479311186c3
Roben Kleene <contact@robenkleene.com>
2020-10-14 17:49:51 -0400

    Add debug

--- src/repla.js
+++ src/repla.js
@@ -2,10 +2,14 @@ import Constants from "../src/constants.js";
 import { spawnSync } from "child_process";
 
 const Repla = {
+  debug: false,
   createWindow: function () {
     return this.runAppleScript([Constants.CREATE_WINDOW_SCRIPT]);
   },
   runAppleScript: function (args) {
+    if (this.debug) {
+      console.log(Constants.APPLESCRIPT_COMMAND, args);
+    }
     const result = spawnSync(Constants.APPLESCRIPT_COMMAND, args);
     return result.stdout;
   },
