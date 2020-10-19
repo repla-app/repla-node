6600cefb7fcd68408dadeb5da6d6f432a001d855
Roben Kleene <contact@robenkleene.com>
2020-10-19 18:10:04 -0400

    Run autocorrect

--- src/test.js
+++ src/test.js
@@ -1,10 +1,14 @@
 import Constants from "../src/constants.js";
 
 const Test = {
-  blockUntil: async function (timeout, pollingInterval = Constants.POLLING_INTERVAL, testFn) {
+  blockUntil: async function (
+    timeout,
+    pollingInterval = Constants.POLLING_INTERVAL,
+    testFn
+  ) {
     let cycles = Math.max(timeout / pollingInterval, 1);
     for (var i = 0; i <= cycles || testFn(); i++) {
-      await new Promise(r => setTimeout(r, pollingInterval));
+      await new Promise((r) => setTimeout(r, pollingInterval));
     }
   },
 };
