5371264b529cbb8a8a59014b9621fd3af2369443
Roben Kleene <contact@robenkleene.com>
2020-10-19 18:09:43 -0400

    Work on block until

--- src/test.js
+++ src/test.js
@@ -1,8 +1,12 @@
+import Constants from "../src/constants.js";
 
 const Test = {
-  blockUntil: function(timeout, pollingInterval) {
-    let cycles = Math.max(timeout / pollingInterval, 1)
-  }
-}
+  blockUntil: async function (timeout, pollingInterval = Constants.POLLING_INTERVAL, testFn) {
+    let cycles = Math.max(timeout / pollingInterval, 1);
+    for (var i = 0; i <= cycles || testFn(); i++) {
+      await new Promise(r => setTimeout(r, pollingInterval));
+    }
+  },
+};
 
 export default Test;
