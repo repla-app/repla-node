7cf3974bb97a2df61da9dc9c5de3b26555bbcf11
Roben Kleene <contact@robenkleene.com>
2020-10-20 17:22:19 -0400

    Add some script constants

--- src/constants.js
+++ src/constants.js
@@ -9,9 +9,9 @@ Constants.POLLING_INTERVAL = "0.5";
 
 // AppleScript
 
-import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const APPLESCRIPT_DIRECTORY = path.join(__dirname, "lib/applescript");
 Constants.APPLESCRIPT_COMMAND = "/usr/bin/osascript";
@@ -22,8 +22,18 @@ Constants.CREATE_WINDOW_SCRIPT = path.join(
 Constants.CLOSE_WINDOW_SCRIPT = path.join(
   APPLESCRIPT_DIRECTORY,
   "close_window.scpt"
-);   
-
-
+);
+Constants.LOAD_URL_SCRIPT = path.join(APPLESCRIPT_DIRECTORY, "load_url.scpt");
+Constants.LOAD_URL_CACHE_SCRIPT = path.join(
+  APPLESCRIPT_DIRECTORY,
+  "load_url_clearing_cache.scpt"
+);
+Constants.DO_JAVASCRIPT = path.join(
+  APPLESCRIPT_DIRECTORY,
+  "do_javascript.scpt"
+);
+Constants.RELOAD_SCRIPT = path.join(APPLESCRIPT_DIRECTORY, "reload.scpt");
+Constants.GO_BACK_SCRIPT = path.join(APPLESCRIPT_DIRECTORY, "go_back.scpt");
+Constants.GO_FORWARD = path.join(APPLESCRIPT_DIRECTORY, "go_forward.scpt");
 
 export default Constants;
