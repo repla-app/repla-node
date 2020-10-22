0564451bb1b7412647b68073c3a83e61dfd1b6cc
Roben Kleene <contact@robenkleene.com>
2020-10-22 17:33:54 -0400

    Add async

--- test/window.test.js
+++ test/window.test.js
@@ -19,7 +19,7 @@ test("Open and close a window", () => {
   window.close();
 });
 
-test("Load a file and URL", () => {
+test("Load a file and URL", done => {
   Repla.loadPlugin(TestConstants.SERVER_PLUGIN_FILE);
   let windowId = Repla.runPlugin(
     TestConstants.SERVER_PLUGIN_NAME,
@@ -30,9 +30,11 @@ test("Load a file and URL", () => {
   fs.readFile(TestConstants.TITLE_JS_FILE, null, (err, data) => {
     if (err) {
       console.error(err);
+      done(err);
       return;
     }
     console.log(data);
+    done();
   });
   window.close();
 });
