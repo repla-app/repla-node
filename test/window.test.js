14f5983320125a328190206acc2de988038c2298
Roben Kleene <contact@robenkleene.com>
2020-10-29 17:36:31 -0400

    Re-organize tests

--- test/window.test.js
+++ test/window.test.js
@@ -4,34 +4,62 @@ const Window = require("../src/window.js");
 const Repla = require("../src/repla.js");
 const fs = require("fs");
 
-test("Sets window ID from environment", () => {
-  const windowId = 1;
-  process.env[Constants.WINDOW_ID_KEY] = windowId;
-  const window = new Window();
-  expect(window.windowId).toBe(windowId);
-  delete process.env[Constants.WINDOW_ID_KEY];
+describe('Nothing', () => {
+  test("Sets window ID from environment", () => {
+    const windowId = 1;
+    process.env[Constants.WINDOW_ID_KEY] = windowId;
+    const window = new Window();
+    expect(window.windowId).toBe(windowId);
+    delete process.env[Constants.WINDOW_ID_KEY];
+  });  
 });
 
-test("Open and close a window", () => {
-  const window = new Window();
-  const windowId = window.windowId;
-  expect(windowId).not.toBeNull();
-  window.close();
+describe('Make window', () => {
+  let window;
+  beforeEach(() => {
+    window = new Window();
+  });
+
+  afterEach(() => {
+    window.close();
+  });
+  
+  test("Open and close a window", () => {
+    const windowId = window.windowId;
+    expect(windowId).not.toBeNull();
+  });
 });
 
-test("Load a file and URL", () => {
-  Repla.debug = true; // FIXME:
-  Repla.loadPlugin(TestConstants.SERVER_PLUGIN_FILE);
-  const windowId = Repla.runPlugin(
-    TestConstants.SERVER_PLUGIN_NAME,
-    TestConstants.HTML_DIRECTORY
-  );
-  const window = new Window(windowId);
-  expect(window.windowId).toBe(windowId);
-  const titleJS = fs.readFileSync(TestConstants.TITLE_JS_FILE).toString();
-  window.loadFile(TestConstants.HTML_FILE);
-  const result = window.doJavaScript(titleJS);
-  // expect(result).toBe(TestConstants.HTML_TITLE);
-  window.close();
-  Repla.debug = false; // FIXME:
+// describe('Load plugin and make window', () => {
+// });
+
+describe('Load plugin and make window', () => {
+  let window;
+  beforeEach(() => {
+    Repla.debug = true; // FIXME:
+    Repla.loadPlugin(TestConstants.SERVER_PLUGIN_FILE);
+    const windowId = Repla.runPlugin(
+      TestConstants.SERVER_PLUGIN_NAME,
+      TestConstants.HTML_DIRECTORY
+    );
+    window = new Window(windowId);
+    expect(window.windowId).toBe(windowId);  
+  });
+
+  afterEach(() => {
+    window.close();
+    Repla.debug = false; // FIXME:
+  });
+
+  test("Load a file and URL", () => {
+    const titleJS = fs.readFileSync(TestConstants.TITLE_JS_FILE).toString();
+    window.loadFile(TestConstants.HTML_FILE);
+    const result = window.doJavaScript(titleJS);
+    // FIXME: Why don't these values match?
+    console.log("result = " + result);
+    console.log("TestConstants.HTML_TITLE = " + TestConstants.HTML_TITLE);
+    // expect("Index A").toBe("Index B");
+    expect(result.toString()).toBe(TestConstants.HTML_TITLE.toString());
+  });  
 });
+
