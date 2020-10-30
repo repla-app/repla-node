23dc6d488c5160cc71badc0af8c956861d96551b
Roben Kleene <contact@robenkleene.com>
2020-10-30 17:39:35 -0400

    Setup reload test

--- test/window.test.js
+++ test/window.test.js
@@ -32,6 +32,7 @@ describe("Make window", () => {
 
 describe("Load plugin and make window", () => {
   let window;
+  const titleJS = fs.readFileSync(TestConstants.TITLE_JS_FILE).toString();
   beforeEach(() => {
     Repla.loadPlugin(TestConstants.SERVER_PLUGIN_FILE);
     const windowId = Repla.runPlugin(
@@ -47,12 +48,18 @@ describe("Load plugin and make window", () => {
   });
 
   test("Load a file and URL", () => {
-    const titleJS = fs.readFileSync(TestConstants.TITLE_JS_FILE).toString();
     window.loadFile(TestConstants.HTML_FILE);
     let result = window.doJavaScript(titleJS);
     expect(result).toBe(TestConstants.HTML_TITLE);
     window.loadURL(TestConstants.HTML_JQUERY_URL)
+  });
+
+  test("Reload", () => {
+    const options = {
+      "shouldClearCache": true
+    }
+    window.loadURL(TestConstants.HTML_URL, options)
     result = window.doJavaScript(titleJS);
-    expect(result).toBe(TestConstants.HTML_JQUERY_TITLE);
+    expect(result).toBe(TestConstants.HTML_TITLE);
   });
 });
